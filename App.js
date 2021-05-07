dd78903e0f2a48f440b5025aaa59fb87aa8cb834
Jiana Jain <jiana2107@gmail.com>
2021-05-07 15:54:37 +0530

    chunks

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity,Image } from 'react-native';
+import {Header} from 'react-native-elements'
+import db from './localdb'
 
-export default function App() {
+//console.log(db["the"].chunks)
+
+export default class App extends React.Component{
+  constructor(){
+    super();
+    this.state={text:'', chunks: []}
+  }
+ 
+  render(){
   return (
+    <SafeAreaProvider>
+      <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        />
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image  style={{width:200,height:200,margin:50}} source={require("./assets/phonetics.jpg")}/>
+
+      <TextInput style={{padding:7,borderWidth:2,color:'white'}}
+      placeholder='Enter Text'
+      onChangeText={text => {this.setState({text:text})}}
+      value={this.state.text}
+      />
+
+      <View>
+        <TouchableOpacity style={{margin:20,borderRadius:10,padding:7,backgroundColor:'dodgerblue'}} 
+          onPress={() => {
+            this.setState({ chunks: db[this.state.text].chunks });
+          }}>
+          <Text>GO</Text>
+        </TouchableOpacity>
+      </View>
+
+      {this.state.chunks.map(item=>{
+        return(
+          <TouchableOpacity style={{margin:20,borderRadius:10,padding:7,backgroundColor:'dodgerblue'}} 
+          onPress={{}}>
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        )  
+      })}
+       
     </View>
+    </SafeAreaProvider>
   );
 }
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    
+    color:"white"
   },
 });
